edb150e1393c21222cb54f55ec9fbbbac2e0474e
Jan Ramdohr <jan.ramdohr@outlook.com>
2025-05-02 20:42:39 +0200

    adjust values

--- next.config.mjs
+++ next.config.mjs
@@ -10,9 +10,8 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  assetPrefix: isProd ? "/your-repository-name/" : "",
-  basePath: isProd ? "/your-repository-name" : "",
-  output: "export",
+  assetPrefix: isProd ? "/ok-stupid/" : "",
+  basePath: isProd ? "/ok-stupid" : "",
 };
 
 export default nextConfig;
